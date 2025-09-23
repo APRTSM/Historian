6ff3a7d7a12f242b00071de47ebe087c3d17c9c8
sahandmoslemi <sahandmoslemi@gmail.com>
2025-09-23 11:36:18 +0300

    fix

--- rq3_code_clone.py
+++ rq3_code_clone.py
@@ -6,11 +6,11 @@ from utils.benchmark import *
 from utils.utils import *
 from utils.tool import *
 from utils.dataset import *
-from build import init, clean_patches, get_methods, get_patch_processors, get_tool_settings, normalaize_names, deduplicate_patches, report_dataset, get_pairs
+from build import init, clean_patches, get_methods, get_patch_processors, get_tool_settings, normalaize_names, deduplicate_patches
 
 
-class Result:
-    def __init__(self, selected_tool, input_processors=None, input_models=None, input_prompts=None):
+class RQ:
+    def __init__(self, selected_tool):
         # Initial Data
         bugs, developer_patches, tool_patches = init(configure=False)
 
@@ -48,10 +48,39 @@ class Result:
 
         logging.info(f"Selected Tool: {selected_tool}, # Developer Patches: {len(developer_patches)}, # Tool Patches: {len(tool_patches)}, Patch Processors: {self.patch_processors}, Models: {self.models}, Prompts: {self.prompts}, Temperatures: {self.temperatures}")
 
+class RQ3(RQ):
+    def __init__(self, selected_tool, input_processors, input_models, input_prompts):
+        super().__init__(selected_tool)
 
 
 if __name__ == "__main__":
-    pairs = pd.read_pickle("/home/sahand/Desktop/hist-update/Historian/tmp/results/EXP2-tbar-method-hermes3:8b-0.1-llm4cc-similarity_line-patch-identical.pkl")
-    results = Result()
+    input_models = [
+        "magicoder:7b-s-cl",
+        "codellama:7b-instruct",
+        "codellama:13b-instruct",
+        "deepseek-coder:6.7b",
+        "codegemma:7b-instruct",
+        "qwen2.5:7b",
+        "qwen2.5-coder:7b",
+        "yi-coder:9b",
+        "hermes3:8b"
+    ]
+
+    input_prompts = [
+        "llm4cc-clone_type",
+        "llm4cc-integrated",
+        "llm4cc-simple_prompt-semantical",
+        "llm4cc-reasoning-patch-semantical",
+        "llm4cc-similarity_line-patch-semantical",
+        "llm4cc-simple_prompt-identical",
+        "llm4cc-reasoning-patch-identical",
+        "llm4cc-similarity_line-patch-identical"
+    ]
+
+    pairs = pd.read_pickle(os.path.join(TMP_RESULTS_DIR, "EXP2-tbar-method-hermes3:8b-0.1-llm4cc-similarity_line-patch-identical.pkl"))
+    print(pairs)
+    pairs = pd.read_pickle(os.path.join(TMP_RESULTS_DIR, "expert", "EXP2-labeled-tbar.pkl"))
+    print(pairs)
+    rq3 = RQ3(selected_tool="tbar", input_processors=None, input_models=input_models, input_prompts=input_prompts)
 
 
