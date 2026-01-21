7b3dd154d66c172405dc278681bc35e6df48c9a2
sahandmoslemi <sahandmoslemi@gmail.com>
2026-01-21 17:23:55 +0300

    fix

--- tools/SourcererCC/tokenizer-input/src-2/src.java
+++ tools/SourcererCC/tokenizer-input/src-2/src.java
@@ -1,114 +1,62 @@
-    public static String formatPeriod(long startMillis, long endMillis, String format, boolean padWithZeros, 
-            TimeZone timezone) {
-
-        long millis = endMillis - startMillis;
-        if (millis < 28 * DateUtils.MILLIS_PER_DAY) {
-            return formatDuration(millis, format, padWithZeros);
+    public static double linearCombination(final double[] a, final double[] b)
+        throws DimensionMismatchException {
+        final int len = a.length;
+        if (len != b.length) {
+            throw new DimensionMismatchException(len, b.length);
         }
 
-        Token[] tokens = lexx(format);
+            // Revert to scalar multiplication.
 
-        // timezones get funky around 0, so normalizing everything to GMT 
-        // stops the hours being off
-        Calendar start = Calendar.getInstance(timezone);
-        start.setTime(new Date(startMillis));
-        if (millis < 28 * DateUtils.MILLIS_PER_DAY) {
-			return formatDuration(millis, format, padWithZeros);
-		}
-		Calendar end = Calendar.getInstance(timezone);
-        end.setTime(new Date(endMillis));
+        final double[] prodHigh = new double[len];
+        double prodLowSum = 0;
 
-        // initial estimates
-        int milliseconds = end.get(Calendar.MILLISECOND) - start.get(Calendar.MILLISECOND);
-        int seconds = end.get(Calendar.SECOND) - start.get(Calendar.SECOND);
-        int minutes = end.get(Calendar.MINUTE) - start.get(Calendar.MINUTE);
-        int hours = end.get(Calendar.HOUR_OF_DAY) - start.get(Calendar.HOUR_OF_DAY);
-        int days = end.get(Calendar.DAY_OF_MONTH) - start.get(Calendar.DAY_OF_MONTH);
-        int months = end.get(Calendar.MONTH) - start.get(Calendar.MONTH);
-        int years = end.get(Calendar.YEAR) - start.get(Calendar.YEAR);
+        for (int i = 0; i < len; i++) {
+            final double ai = a[i];
+            final double ca = SPLIT_FACTOR * ai;
+            final double aHigh = ca - (ca - ai);
+            final double aLow = ai - aHigh;
 
-        // each initial estimate is adjusted in case it is under 0
-        while (milliseconds < 0) {
-            milliseconds += 1000;
-            seconds -= 1;
+            final double bi = b[i];
+            final double cb = SPLIT_FACTOR * bi;
+            final double bHigh = cb - (cb - bi);
+            final double bLow = bi - bHigh;
+            prodHigh[i] = ai * bi;
+            final double prodLow = aLow * bLow - (((prodHigh[i] -
+                                                    aHigh * bHigh) -
+                                                   aLow * bHigh) -
+                                                  aHigh * bLow);
+            prodLowSum += prodLow;
         }
-        while (seconds < 0) {
-            seconds += 60;
-            minutes -= 1;
-        }
-        while (minutes < 0) {
-            minutes += 60;
-            hours -= 1;
-        }
-        while (hours < 0) {
-            hours += 24;
-            days -= 1;
-        }
-        while (days < 0) {
-            days += 31;
-//days += 31; // TODO: Need tests to show this is bad and the new code is good.
-// HEN: It's a tricky subject. Jan 15th to March 10th. If I count days-first it is 
-// 1 month and 26 days, but if I count month-first then it is 1 month and 23 days.
-// Also it's contextual - if asked for no M in the format then I should probably 
-// be doing no calculating here.
-            months -= 1;
-        }
-        while (months < 0) {
-            months += 12;
-            years -= 1;
-        }
-        milliseconds -= reduceAndCorrect(start, end, Calendar.MILLISECOND, milliseconds);
-        seconds -= reduceAndCorrect(start, end, Calendar.SECOND, seconds);
-        minutes -= reduceAndCorrect(start, end, Calendar.MINUTE, minutes);
-        hours -= reduceAndCorrect(start, end, Calendar.HOUR_OF_DAY, hours);
-        days -= reduceAndCorrect(start, end, Calendar.DAY_OF_MONTH, days);
-        months -= reduceAndCorrect(start, end, Calendar.MONTH, months);
-        years -= reduceAndCorrect(start, end, Calendar.YEAR, years);
 
-        // This next block of code adds in values that 
-        // aren't requested. This allows the user to ask for the 
-        // number of months and get the real count and not just 0->11.
-        if (!Token.containsTokenWithValue(tokens, y)) {
-            if (Token.containsTokenWithValue(tokens, M)) {
-                months += 12 * years;
-                years = 0;
-            } else {
-                // TODO: this is a bit weak, needs work to know about leap years
-                days += 365 * years;
-                years = 0;
+
+        final double prodHighCur = prodHigh[0];
+//ACS's patch begin
+	if (len==1.0){return a[0] * b[0];}
+//ACS's patch end
+        double prodHighNext = prodHigh[1];
+        double sHighPrev = prodHighCur + prodHighNext;
+        double sPrime = sHighPrev - prodHighNext;
+        double sLowSum = (prodHighNext - (sHighPrev - sPrime)) + (prodHighCur - sPrime);
+
+        final int lenMinusOne = len - 1;
+        for (int i = 1; i < lenMinusOne; i++) {
+            prodHighNext = prodHigh[i + 1];
+            final double sHighCur = sHighPrev + prodHighNext;
+            sPrime = sHighCur - prodHighNext;
+            sLowSum += (prodHighNext - (sHighCur - sPrime)) + (sHighPrev - sPrime);
+            sHighPrev = sHighCur;
+        }
+
+        double result = sHighPrev + (prodLowSum + sLowSum);
+
+        if (Double.isNaN(result)) {
+            // either we have split infinite numbers or some coefficients were NaNs,
+            // just rely on the naive implementation and let IEEE754 handle this
+            result = 0;
+            for (int i = 0; i < len; ++i) {
+                result += a[i] * b[i];
             }
         }
-        if (!Token.containsTokenWithValue(tokens, M)) {
-            days += end.get(Calendar.DAY_OF_YEAR) - start.get(Calendar.DAY_OF_YEAR);
-            months = 0;
-        }
-        if (!Token.containsTokenWithValue(tokens, d)) {
-            hours += 24 * days;
-            days = 0;
-        }
-        if (!Token.containsTokenWithValue(tokens, H)) {
-            minutes += 60 * hours;
-            hours = 0;
-        }
-        if (!Token.containsTokenWithValue(tokens, m)) {
-            seconds += 60 * minutes;
-            minutes = 0;
-        }
-        if (!Token.containsTokenWithValue(tokens, s)) {
-            milliseconds += 1000 * seconds;
-            seconds = 0;
-        }
 
-        return format(tokens, years, months, days, hours, minutes, seconds, milliseconds, padWithZeros);
-    }
-    static int reduceAndCorrect(Calendar start, Calendar end, int field, int difference) {
-        end.add( field, -1 * difference );
-        int endValue = end.get(field);
-        int startValue = start.get(field);
-        if (endValue < startValue) {
-            int newdiff = startValue - endValue;
-            return newdiff;
-        } else {
-            return 0;
-        }
+        return result;
     }
